6d9bfab7d7fbe615b0dcd0cf6f1f983bcd9c234c
Adam H <183345857+adamhaouam@users.noreply.github.com>
2025-12-06 18:02:36 +1100

    add event listener to each menu button
    create functions for each respective menu button and link them from the
    createPage.js module to the main module

--- src/createPage.js
+++ src/createPage.js
@@ -1,3 +1,14 @@
-console.log("Generating page 1!");
 
-export default "Generated!";
+
+export function createPage1() {
+    console.log("Generating page 1!");
+};
+
+export function createPage2() {
+    console.log("Generating page 2!");
+};
+
+export function createPage3() {
+    console.log("Generating page 3!");
+};
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import outcome from "./createPage.js";
+import { createPage1, createPage2, createPage3 } from "./createPage.js";
 import "./styles.css";
 import '../node_modules/modern-normalize';
 
-console.log("Index ran!:", outcome);
+const button1 = document.getElementById("menuButton1");
+const button2 = document.getElementById("menuButton2");
+const button3 = document.getElementById("menuButton3");
+
+button1.addEventListener("click", createPage1);
+button2.addEventListener("click", createPage2);
+button3.addEventListener("click", createPage3);
+
+
